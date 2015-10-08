10097d142971ffd557b4ff02027c2598168e17bf
Zhou Zejin <zjzhou1@chinaums.com>
2015-10-08 10:07:31 +0800

    完成5.2.1 使用Intent Filter为隐式Intent提供服务-4.选择联系人实例

--- gen/com/sunny/contactpicker/R.java
+++ gen/com/sunny/contactpicker/R.java
@@ -25,18 +25,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int contactListView=0x7f080000;
+        public static final int itemTextView=0x7f080003;
+        public static final int pick_contact_button=0x7f080002;
+        public static final int selected_contact_textview=0x7f080001;
     }
     public static final class layout {
         public static final int activity_contact_picker=0x7f030000;
+        public static final int activity_contactpicker_tester=0x7f030001;
+        public static final int item_contacter=0x7f030002;
     }
     public static final class menu {
         public static final int contact_picker=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
--- src/com/sunny/contactpicker/ContactPickerActivity.java
+++ src/com/sunny/contactpicker/ContactPickerActivity.java
@@ -1,9 +1,17 @@
 package com.sunny.contactpicker;
 
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class ContactPickerActivity extends Activity {
 
@@ -11,24 +19,35 @@ public class ContactPickerActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contact_picker);
+		
+		final Cursor cursor = getContentResolver().query(
+				ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+		
+		String[] from = new String[] { Contacts.DISPLAY_NAME_PRIMARY };
+		int[] to = new int[] { R.id.itemTextView };
+		
+		@SuppressWarnings("deprecation")
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+				this, R.layout.item_contacter, cursor, from, to);
+		ListView lv = (ListView) findViewById(R.id.contactListView);
+		lv.setAdapter(adapter);
+		
+		lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				cursor.moveToPosition(position);
+				int rowId = cursor.getInt(cursor.getColumnIndex("_id"));
+				Uri outURI = ContentUris
+						.withAppendedId(ContactsContract.Contacts.CONTENT_URI, rowId);
+				
+				Intent outData = new Intent();
+				outData.setData(outURI);
+				setResult(Activity.RESULT_OK, outData);
+				finish();
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.contact_picker, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
